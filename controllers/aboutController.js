8950ac773aaae97c06a9da836c05d530c7448a92
Iasonas Lamprinidis <iasonasl@ece.auth.gr>
2026-01-08 20:15:46 +0200

    test fix violation

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -5,12 +5,12 @@ import { sendSuccess } from '../utils/responses.js';
  * GET /about-us/
  * Returns About Us paragraph.
  *
- * @param {import('express').Request} _req
+ * @param {import('express').Request} _
  * @param {import('express').Response} res
  * @param {Function} next
  * @returns {Promise<void>}
  */
-export const getAboutUs = async (_req, res, next) => {
+export const getAboutUs = async (_, res, next) => {
   try {
     const paragraph = await getAboutUsText();
     sendSuccess(res, { paragraph }, 'About Us content retrieved.');
